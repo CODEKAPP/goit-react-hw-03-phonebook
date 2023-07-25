c2f8df2a47db9d2d785d8ecbf76ea80c16a786d9
CODEKAPP <117596036+CODEKAPP@users.noreply.github.com>
2023-07-25 18:27:07 -0500

    Commit03

--- src/components/Contacts/Filter.jsx
+++ src/components/Contacts/Filter.jsx
@@ -1,4 +1,3 @@
-// Filter.jsx
 import React from 'react';
 import { FilterStyleContainer } from './Styles/FormStyles';
 import { InputStyle } from './Styles/InputStyle';
@@ -9,16 +8,38 @@ const Filter = ({ filter, setFilter }) => {
     setFilter(e.target.value);
   };
 
+  const handleClearFilter = () => {
+    setFilter('');
+  };
+
   return (
     <FilterStyleContainer>
       <LabelStyle htmlFor="filter">Find contacts by name</LabelStyle>
-      <InputStyle
-        type="text"
-        name="filter"
-        placeholder="Search..."
-        value={filter}
-        onChange={handleFilterChange}
-      />
+      <div style={{ position: 'relative' }}>
+        <InputStyle
+          type="text"
+          name="filter"
+          placeholder="Search..."
+          value={filter}
+          onChange={handleFilterChange}
+        />
+        {filter && (
+          <button
+            onClick={handleClearFilter}
+            style={{
+              position: 'absolute',
+              top: '50%',
+              right: '10px',
+              transform: 'translateY(-50%)',
+              cursor: 'pointer',
+              border: 'none',
+              background: 'transparent',
+            }}
+          >
+            <span style={{ fontSize: '14px', fontWeight: 'bold' }}>X</span>
+          </button>
+        )}
+      </div>
     </FilterStyleContainer>
   );
 };
